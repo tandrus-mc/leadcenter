2af6bf35edeeeaf5a745714ab564c5c312a87622
Tim <tandrus@mcclainconcepts.com>
2016-04-27 07:51:19 -0700

    started csv validation

--- app/Http/Controllers/CsvValidationController.php
+++ app/Http/Controllers/CsvValidationController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Validator;
 use League\Csv\Reader;
 use League\Csv\Writer;
 
@@ -20,10 +21,18 @@ class CsvValidationController extends Controller
     protected $dest_path;
     protected $source_path;
 
-    protected $csvWriter;
-    protected $csvReader;
+    public $csvWriter;
+    public $csvReader;
 
     protected $leadList;
+
+    protected $rowLimit;
+
+    protected $errors;
+
+    protected $currentRow;
+
+    protected $validHeader;
 
     public function __construct(){
         $this->server      = 'clientftp.xverify.com';
@@ -34,6 +43,7 @@ class CsvValidationController extends Controller
         $this->source_path = storage_path('app/csv/');
         $this->dest_path   = '/incoming/email/';
 
+        $this->currentRow  = 1;
 
     }
 
@@ -43,7 +53,11 @@ class CsvValidationController extends Controller
         $this->csvWriter   = csvWriter($this->leadList->csvPath().$this->leadList->file_name);
         $this->csvReader   = readerFromPath($this->leadList->path);
 
+
+
         $this->prepCsvForTransfer();
+
+        dd($this->errors);
 
         $connection = ftp_connect($this->server);
 
@@ -61,7 +75,51 @@ class CsvValidationController extends Controller
         ftp_close($connection);
     }
 
-    protected function prepCsvForTransfer(){
-       $this->csvWriter->insertAll($this->csvReader);
+    public function prepCsvForTransfer(){
+
+            //add csvValidators
+
+            //header validation
+            /*$this->csvWriter->addValidator(function (array $header) {
+                $headerValidator = Validator::make($header, [
+                    //pass header validation rules
+                ]);
+                if($headerValidator->fails()){
+                    $this->errors['incorrect_format_header'] = $headerValidator;
+                    $pass = false;
+                } else{
+                    $pass = true;
+                }
+                return $pass;
+            }, 'incorrect_format_header');
+
+            $this->csvWriter->insertOne($this->csvReader->fetchOne(0));
+
+            $this->csvWriter->removeValidator('incorrect_format_header');*/
+
+            //row validation
+            $this->csvWriter->addValidator(function (array $row) {
+                $rowValidator = Validator::make($row, [
+                    'email'         => 'required|email|min:6',
+                    'country'       => 'required',
+                    'capture_date'  => 'required',
+                    'ip'            => 'required'
+                ]);
+                if($rowValidator->fails()){
+                    $this->errors['incorrect_format_row_'.$this->currentRow++] = $rowValidator;
+                    $pass = false;
+                } else {
+                    $pass = true;
+                }
+                return $pass;
+            }, 'incorrect_format_row');
+
+            foreach($this->csvReader->fetchAll() as $row){
+                try{
+                    $this->csvWriter->insertOne($row);
+                } catch(\Exception $e){
+                    continue;
+                }
+            }
     }
 }
